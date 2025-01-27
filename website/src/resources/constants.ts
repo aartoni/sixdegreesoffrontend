ab2dede07413e5bfff483202e4e4bf4859044e63
aartoni <alessio@artoni.org>
2025-01-27 12:03:36 +0100

    chore: run linter

--- website/src/resources/constants.ts
+++ website/src/resources/constants.ts
@@ -5,8 +5,8 @@ import {HomeLink} from '../types';
 export const API_URL = import.meta.env.VITE_API_URL;
 export const THIRD_PARTY_API_URL = import.meta.env.VITE_THIRD_PARTY_API_URL;
 
-const homeLinkRefs = import.meta.env.VITE_HOME_LINK_REFS?.split(",") ?? [];
-const homeLinkNames = import.meta.env.VITE_HOME_LINK_NAMES?.split(",") ?? [];
+const homeLinkRefs = import.meta.env.VITE_HOME_LINK_REFS?.split(',') ?? [];
+const homeLinkNames = import.meta.env.VITE_HOME_LINK_NAMES?.split(',') ?? [];
 export const HOME_LINKS: HomeLink[] = [];
 
 if (homeLinkRefs.length === homeLinkNames.length) {
