654b9cd034ecdfbd2c5c8f636dc82c12e3e1eedc
aartoni <alessio@artoni.org>
2025-01-28 14:20:23 +0100

    feat: remove blog routes

--- website/src/components/App.tsx
+++ website/src/components/App.tsx
@@ -1,16 +1,11 @@
 import Particles from '@tsparticles/react';
-import React, {lazy, Suspense} from 'react';
+import React from 'react';
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import {ThemeProvider} from 'styled-components';
 
 import particlesConfig from '../resources/particles.config.json';
 import theme from '../resources/theme.json';
 import {Home} from './Home';
-
-const AsyncBlog = lazy(() => import('./blog/Blog').then((module) => ({default: module.Blog})));
-const AsyncBlogPost = lazy(() =>
-  import('./blog/BlogPost').then((module) => ({default: module.BlogPost}))
-);
 
 export const App: React.FC = () => {
   return (
@@ -28,22 +23,6 @@ export const App: React.FC = () => {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route
-              path="/blog"
-              element={
-                <Suspense fallback={null}>
-                  <AsyncBlog />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/blog/:postId"
-              element={
-                <Suspense fallback={null}>
-                  <AsyncBlogPost />
-                </Suspense>
-              }
-            />
             {/* Redirect unmatched routes to home page, replacing history stack. */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
